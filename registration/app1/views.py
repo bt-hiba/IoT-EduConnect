568eac933b73f34cff6e7124e530f68919e993cf
stage <hibabity10@gmail.com>
2024-04-02 12:03:17 +0200

    all projects

--- registration/app1/views.py
+++ registration/app1/views.py
@@ -212,7 +212,7 @@ def IndexPage(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('login')
     else:
         form = ContactForm()
     return render(request, 'index.html', {'form': form})
